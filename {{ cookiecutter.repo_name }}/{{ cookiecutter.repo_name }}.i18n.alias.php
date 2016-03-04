59f5b6a285720f1eaf10afeac760473e2d3199ad
Jonas Gröger <jonas.groeger@posteo.de>
2016-03-05 00:55:13 +0100

    Add make fix and make all tests run through

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}.i18n.alias.php
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}.i18n.alias.php
@@ -1,4 +1,4 @@
-{% if cookiecutter.integration_add_example_special_page %}
+{% if cookiecutter.integration_add_example_special_page == 'y' -%}
 <?php
 /**
  * Aliases for special pages of the {{ cookiecutter.repo_name }} extension
